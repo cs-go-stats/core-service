bb42acfae1582f26ad177f75017f2e9bea9b1c4b
Viktor Liutianskyi <born.to.run.unix@gmail.com>
2020-01-06 00:23:36 +0200

    Implemented base for services startup and runtime. Added common used extensions and abstractions.

---
+++ core-service/Initialization/Runtime.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using CSGOStats.Extensions.Validation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+
+namespace CSGOStats.Services.Core.Initialization
+{
+    internal class Runtime
+    {
+        private readonly ServiceProvider _serviceProvider;
+        private readonly IConfigurationRoot _configuration;
+        private readonly Func<IServiceProvider, Task> _actionBeforeStart;
+        private readonly Func<IServiceProvider, Task> _actionBeforeFinish;
+
+        public Runtime(
+            ServiceProvider serviceProvider,
+            IConfigurationRoot configuration,
+            Func<IServiceProvider, Task> actionBeforeStart,
+            Func<IServiceProvider, Task> actionBeforeFinish)
+        {
+            _serviceProvider = serviceProvider.NotNull(nameof(serviceProvider));
+            _configuration = configuration.NotNull(nameof(configuration));
+            _actionBeforeStart = actionBeforeStart;
+            _actionBeforeFinish = actionBeforeFinish;
+        }
+
+        public async Task RunAsync()
+        {
+            try
+            {
+                if (_actionBeforeStart != null)
+                {
+                    await _actionBeforeStart(_serviceProvider);
+                }
+
+                await TryStartSchedulerAsync();
+                await Task.Delay(-1);
+            }
+            finally
+            {
+                if (_actionBeforeFinish != null)
+                {
+                    await _actionBeforeFinish(_serviceProvider);
+                }
+
+                await TryStopSchedulerAsync();
+                await _serviceProvider.DisposeAsync();
+            }
+        }
+
+        private async Task TryStartSchedulerAsync()
+        {
+            var scheduler= GetScheduler();
+            if (scheduler == null)
+            {
+                return;
+            }
+
+            var setup = _serviceProvider.GetService<Func<IScheduler, IServiceProvider, IConfigurationRoot, Task>>();
+            await setup(scheduler, _serviceProvider, _configuration);
+        }
+
+        private Task TryStopSchedulerAsync() =>
+            GetScheduler()?.Shutdown(false) ?? Task.CompletedTask;
+
+        private IScheduler GetScheduler() =>
+            _serviceProvider.GetService<IScheduler>();
+    }
+}
---
+++ core-service/Links/HltvLinkExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CSGOStats.Services.Core.Links
+{
+    public static class HltvLinkExtensions
+    {
+        private const string HltvRoot = "https://hltv.org";
+
+        public static string HltvUri(this string x) => HltvRoot + x;
+    }
+}
